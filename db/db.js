4298d456b115025ac223319a4ac85809737670c7
DarkHonin <darkhonin@gmail.com>
2020-12-05 16:00:26 +0200

    DB base

---
+++ db/db.js
@@ -0,0 +1,8 @@
+var mysql = require('mysql')
+
+module.exports = db = mysql.createConnection({
+    host: 'ixnzh1cxch6rtdrx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'bw2jn7xd5czsmio9',
+    password: 'wk923ds9c85t0fy8',
+    database: 'nauj37xe35r0e6ar'
+  })
