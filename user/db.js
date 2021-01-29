a3ceafe41eb33b9c18856ba15bc47bb5c6efce0e
DarkHonin <darkhonin@gmail.com>
2021-01-29 16:11:37 +0200

    removed roles

---
+++ user/db.js
@@ -0,0 +1,57 @@
+
+var mongoose = require('mongoose')
+const db = mongoose.connection
+const crypto = require('crypto')
+
+const abstractUser = {
+    uname : {type: String, required : true},
+    hash : String,
+    meta : {},
+    role : {
+        type : String,
+        enum : ['user', 'admin'],
+        default : 'visitor'
+    }
+  }
+
+const userSchema = new mongoose.Schema(abstractUser, { timestamps: true });
+
+userSchema.methods.identity = function(){
+    var object = this.toObject();
+    delete object.hash
+    delete object.meta
+    console.log(object)
+    return object
+}
+
+userSchema.methods.login = async function(password){
+    return new Promise((resolve, reject) => {
+        const [salt, key] = this.hash.split(":")
+        crypto.scrypt(password, salt, 64, (err, derivedKey) => {
+            if (err) reject(err);
+            resolve(key == derivedKey.toString('hex'))
+        });
+    })
+}
+
+userSchema.statics.custom = async function({username, password, meta = {}, role}){
+    return new Promise((resolve, reject) => {
+        // generate random 16 bytes long salt
+        const salt = crypto.randomBytes(16).toString("hex")
+
+        crypto.scrypt(password, salt, 64, (err, derivedKey) => {
+            if (err) reject(err);
+            resolve(salt + ":" + derivedKey.toString('hex'))
+        });
+    }).then(hash => new abstractUserModel({uname : username, hash, meta, role}).save())
+}
+
+userSchema.statics.has_admin = function(){
+    return abstractUserModel.findOne({role : "admin"}).then(admin => {
+        return admin && admin.role === 'admin'
+    })
+}
+
+const userModel = mongoose.model('user', userSchema)
+
+module.exports = {userSchema, userModel}
